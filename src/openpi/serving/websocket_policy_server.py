45184b6c11f2d8111fe147d0c535cd8669502955
phoebehxf <h_xf@outlook.com>
2026-08-15 09:50:25 +0100

    debug bci-pipeline

--- src/openpi/serving/websocket_policy_server.py
+++ src/openpi/serving/websocket_policy_server.py
@@ -41,6 +41,7 @@ class WebsocketPolicyServer:
             self._port,
             compression=None,
             max_size=None,
+            ping_interval=None,
             process_request=_health_check,
         ) as server:
             await server.serve_forever()
